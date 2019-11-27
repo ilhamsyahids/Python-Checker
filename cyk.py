0d9c868051e183bb05ea740832658c81078956d1
Ilham Syahid S <ilhamsyahids@gmail.com>
2019-11-27 11:23:04 +0700

    LS
    Co-authored-by: okugatafahmi <otasrit@gmail.com>

--- cyk.py
+++ cyk.py
@@ -5,21 +5,21 @@ dp = {}
 
 
 def nonTerminalOf(X):
-# mencari nonterminal dari suatu terminal input
-    if (X[0][0]>='0' and X[0][0]<='9'):
-        return ['NUMBER']
+    # mencari nonterminal dari suatu terminal input
+    if (X[0][0] >= '0' and X[0][0] <= '9'):
+        return nonTerminalOf(['number'])
     resNonTerminal = []
     for key, val in rule.items():
         if (X in val):
             resNonTerminal.append(key)
-    if (resNonTerminal==[]):
-        return ['OBJECT']
+    if (resNonTerminal == []):
+        return nonTerminalOf(['object'])
     else:
         return resNonTerminal
 
 
 def searchNonTerminal(X):
-# mencari nonterminal dari suatu nonterminal
+    # mencari nonterminal dari suatu nonterminal
     res = []
     # print("cok",X)
     for key, lol in rule.items():
@@ -30,21 +30,21 @@ def searchNonTerminal(X):
     return res
 
 
-def makeIsi(pos1,pos2):
-# mengembalikan list nonterminal hasil penggabungan 2 sel
+def makeIsi(pos1, pos2):
+    # mengembalikan list nonterminal hasil penggabungan 2 sel
     # print('(',pos1[0],pos1[1],') (',pos2[0],pos2[1],')')
     res = []
     for val1 in tabel[pos1[0]][pos1[1]]:
         for val2 in tabel[pos2[0]][pos2[1]]:
-            if (val1==[] or val2==[]): 
+            if (val1 == [] or val2 == []):
                 continue
-            temp = (val1,val2)
+            temp = (val1, val2)
             if (temp in dp):
                 temp = dp[temp]
             else:
-                temp = searchNonTerminal([val1,val2])
-                dp[(val1,val2)] = temp
-            if (temp!=[]):
+                temp = searchNonTerminal([val1, val2])
+                dp[(val1, val2)] = temp
+            if (temp != []):
                 res += temp
     return res
 
@@ -54,40 +54,46 @@ def cyk():
     # Mencari nonterminal yang menghasilkan terminal input
     for j in range(banyakTerminal):
         tabel[0][j] = nonTerminalOf([terminalInput[j]])
-        print(tabel[0][j],end=' ')
+        print(tabel[0][j], end=' ')
     print()
 
     for row in range(1, banyakTerminal):
         for col in range(banyakTerminal-row):
             # print(row,' ',col,':',end=' ')
-            for r in range(row-1,-1,-1):
-                pos1 = (r,col)
-                pos2 = (row-r-1,r+col+1)
+            for r in range(row-1, -1, -1):
+                pos1 = (r, col)
+                pos2 = (row-r-1, r+col+1)
                 # Mencari hasil tabel[row][col]
-                temp = makeIsi(pos1,pos2)
+                temp = makeIsi(pos1, pos2)
                 if not(temp in tabel[row][col]):
-                    tabel[row][col]+=temp
+                    tabel[row][col] += temp
             tabel[row][col] = list(dict.fromkeys(tabel[row][col]))
 
+
 def printTabel():
-    for row in range(banyakTerminal-1,-1,-1):
-        for col in range(0,banyakTerminal-row):
-            print(tabel[row][col],end=' ')
+    for row in range(banyakTerminal-1, -1, -1):
+        for col in range(0, banyakTerminal-row):
+            print(tabel[row][col], end=' ')
         print()
 
-def python_checker():
-    global terminalInput,banyakTerminal, tabel, rule
 
-    namaFile = input('Masukkan nama file: ')
+def python_checker():
+    global terminalInput, banyakTerminal, tabel, rule
+
+    # namaFile = input('Masukkan nama file: ')
+    namaFile = 'coba.txt'
     terminalInput = readFile.inputFromText(namaFile)
     print(terminalInput)
     banyakTerminal = len(terminalInput)
-    
-    rule = grammar.makeRule('test.txt') # file grammar
-    tabel = [[[] for j in range(banyakTerminal)] for i in range(banyakTerminal)]
-    
+
+    rule = grammar.makeRule('grammarcoba.txt')  # file grammar
+    for k, v in rule.items():
+        print(k, v)
+    tabel = [[[] for j in range(banyakTerminal)]
+             for i in range(banyakTerminal)]
+
     cyk()
-    
+
     print('\nIni hasil')
     printTabel()
     if ('S' in tabel[banyakTerminal-1][0]):
@@ -96,7 +102,7 @@ def python_checker():
         print('Syntax Error')
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     # print('Ini rule')
     # for k,v in sorted(rule.items()):
     #     print(k,v)
--- readFile.py
+++ readFile.py
@@ -8,29 +8,31 @@ def inputFromText(namaFile):
         print(line)
         isNumber = False
         for cc in line:
-            if (cc==" " or (isNumber and (cc<'0' or cc>'9'))):
+            if (cc == " " or (isNumber and (cc < '0' or cc > '9'))):
                 if (isNumber):
                     isNumber = False
-                hasil.append(temp)
-                if (cc==" "):
+                if (temp != ""):
+                    hasil.append(temp)
+                if (cc == " "):
                     temp = ""
                 else:
                     temp = cc
             elif (cc in terminal):
-                if (temp!=""):
+                if (temp != ""):
                     hasil.append(temp)
                 hasil.append(cc)
                 temp = ""
             else:
-                if (not isNumber and (cc>='0' and cc<='9') and temp==""):
+                if (not isNumber and (cc >= '0' and cc <= '9') and temp == ""):
                     isNumber = True
                 temp = temp + cc
-        if (temp!=""):
+        if (temp != ""):
             hasil.append(temp)
         hasil.append('\\n')
     hasil.pop()
     return hasil
-        
+
+
 def getTerminal(namaFile):
     fin = open(namaFile)
     allTerminals = []
